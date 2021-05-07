a98df9b510b29f1921b57678982d9fd0ac4affe5
SergioTrinquet <serge.trinquet@gmail.com>
2021-05-07 11:09:33 +0200

    Integration MongoDB + Mongoose

--- API/index.js
+++ API/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3080;
 
+const config = require("./config/mongoDB.js");
+const mongoose = require('mongoose');
+const Department = require('./models/department');
+const Product = require('./models/product');
+
 const mockData = require('./mockData/mock'); // Variables mock juste pour phase de dev.
 
 const erreur = require('./app_modules/erreur');
@@ -15,10 +20,35 @@ app.use((req, res, next) => {
     next();
 });
 
+
+// Chaine de connexion à MongoDB
+//console.log(config.username + " " + config.password + " " + config.db); //TEST
+const dbURI = `mongodb+srv://${config.username}:${config.password}@clustershopping.z06ey.mongodb.net/${config.db}?retryWrites=true&w=majority`;
+// Connection à mongoDB avec la surcouche mangoose
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => {
+        app.listen(port, () => {
+            console.log("J'écoute au port " + port); //TEST
+        })
+    })
+    .catch(err => console.log("Erreur de connexion"));
+
+
+
 // Pour alimenter marge rayons
 app.get("/get_rayons", (req, res, next) => {
     try {
-        res.json(mockData.rayons);
+        // === Version avec Mock ===
+        //res.json(mockData.rayons); 
+
+        // === Version avec data dans mongoDB ===
+        Department.find().sort("intitule")
+        .then(result => {
+            res.json(result);
+            console.log(result); //TEST
+        })
+        .catch(err => console.log("Erreur dans récupération des rayons dans collection mongoDB"));
+
     } catch (error) {
         error.customMsg = "Erreur lors de l'étape du chargement de la liste des rayons";
         next(error);
@@ -29,21 +59,33 @@ app.get("/get_rayons", (req, res, next) => {
 // Pour récupérer les produits d'un rayon
 app.get("/get_produits_rayon/:rayonId", (req, res, next) => {
     try {
-        // ICI renvoyer un tableau d'objets avec des produits correspondant à un 'rayonId'
-        const produitsDuRayon = mockData.produits.filter(p => p.rayon.id == req.params.rayonId);
-        console.log(produitsDuRayon); //TEST
-        res.json(produitsDuRayon); //TEST
+        // Renvoit d'un tableau d'objets avec des produits correspondant à un 'rayonId'
+
+        // === Version avec Mock ===
+        /* const produitsDuRayon = mockData.produits.filter(p => p.rayon.id == req.params.rayonId);
+        res.json(produitsDuRayon); */
+
+        // === Version avec data dans mongoDB ===
+        Product
+            .find({rayon: req.params.rayonId}) // Bonne version !!
+            //.find({"rayon": ObjectId(req.params.rayonId)}) // Mauvaise version
+            .populate("rayon")
+            .then(result => {
+                res.json(result);
+                //console.log(result); //TEST
+            })
+            .catch(err => console.log("Erreur dans récupération des rayons dans collection mongoDB"));
         
-        //const nomRayon = produitsDuRayon(0).rayon.intitule;
-        //res.json({ produitsDuRayon: produitsDuRayon, nomRayon: nomRayon }); //TEST
     } catch (error) {
         error.customMsg = "Erreur lors de l'étape de récupération des produits d'un rayon";
         next(error);
     }
 });
 
+
+app.get("/get_filters", (res, req, next) => {
+
+});
+
 app.use(erreur); // Middleware de gestion des erreurs
 
-app.listen(port, () => {
-    console.log("J'écoute au port " + port); //TEST
-})
