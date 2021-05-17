e282e7dcddb4f1ca056586bd4277562994eb0852
SergioTrinquet <serge.trinquet@gmail.com>
2021-05-17 17:54:33 +0200

    MAJ schema

--- API/models/nutriscore.js
+++ API/models/nutriscore.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// NOTE: Si pas de '_id' dans le schema, création d'une prop. '_id' automatique dans mongoDB quand même, de type ObjectId
+// NOTE suite : Qd prop '_id' dans schema, le créé selon le Type stipulé dedans, donc si Type 'String' par ex., forcera le type par défaut ObjectId
 const nutriscoreSchema = new Schema({
-    /* id: {
+    _id: {
         type: String,
         required: true
-    }, */
+    },
     lettre: {
         type: String,
         required: true
@@ -14,5 +16,7 @@ const nutriscoreSchema = new Schema({
 
 const Nutriscore = mongoose.model('Nutriscore', nutriscoreSchema);
 
-//module.exports = Nutriscore;
-module.exports = {'model': Nutriscore, 'schema': nutriscoreSchema};
+module.exports = {
+    'model': Nutriscore, 
+    'schema': nutriscoreSchema
+};
