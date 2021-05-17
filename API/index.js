448722437349be450ffe667f10ea3a4b48fbc377
SergioTrinquet <serge.trinquet@gmail.com>
2021-05-17 17:51:49 +0200

    Requetes pour '/Filtres' executées en parallèle + creat° API pour recup prd avec filtres

--- API/index.js
+++ API/index.js
@@ -6,7 +6,7 @@ const erreur = require('./app_modules/erreur');
 
 const mockData = require('./mockData/mock'); // Variables mock juste pour phase de dev.
 
-const config = require("./config/mongoDB.js");
+const config = require("./config/identifiants_mongoDB.js");
 const mongoose = require('mongoose');
 const Department = require('./models/department');
 const Product = require('./models/product');
@@ -23,6 +23,11 @@ app.use((req, res, next) => {
     next();
 });
 
+// Fonctions Middleware permettant d'interpréter les paramètres POST 'req.body' 
+// (to support JSON-encoded and URL-encoded bodies)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 // Chaine de connexion à MongoDB
 //console.log(config.username + " " + config.password + " " + config.db); //TEST
@@ -38,8 +43,8 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 
 
-// Pour alimenter marge rayons
-app.get("/get_rayons", (req, res, next) => {
+// Pour alimenter marge 'rayons'
+app.get("/departments", (req, res, next) => {
     // === Version avec Mock === //
     /* try {
         res.json(mockData.rayons); 
@@ -62,7 +67,7 @@ app.get("/get_rayons", (req, res, next) => {
 });
 
 // Pour récupérer les produits d'un rayon
-app.get("/get_produits_rayon/:rayonId", (req, res, next) => {
+app.get("/department_products/:rayonId", (req, res, next) => {
     
     // === Version avec Mock === //
     /* try {
@@ -75,47 +80,110 @@ app.get("/get_produits_rayon/:rayonId", (req, res, next) => {
 
     // === Version avec data dans mongoDB === //
     Product
-    .find({rayon: req.params.rayonId}) // Bonne version !!
-    //.find({"rayon": ObjectId(req.params.rayonId)}) // Mauvaise version
-    .populate("rayon")
-    .then(result => {
-        res.json(result);
-    })
-    .catch(error => { 
-        error.customMsg = "Erreur lors de l'étape de récupération des produits d'un rayon";
-        next(error);
-    });
+        .find({rayon: req.params.rayonId}) // Bonne version !!
+        //.find({"rayon": ObjectId(req.params.rayonId)}) // Mauvaise version
+        .populate("rayon")
+        .then(result => {
+            res.json(result);
+        })
+        .catch(error => { 
+            error.customMsg = "Erreur lors de l'étape de récupération des produits d'un rayon";
+            next(error);
+        });
 
 });
 
 
-// Pour récupérer les filtres (marge gauche qd produits affichés)
-app.get("/get_filters", async (req, res, next) => {
-    
-    try {
-        let filters = {};
-
-        // ESSAYER D'INTERROGER LES TABLES EN PARALLELE !!!
-        const result_nutriscore = await Nutriscore.find().sort('lettre');
-        console.log("Nutriscore ===>", result_nutriscore); //TEST
-        filters.nutriscore = result_nutriscore;
-
-        const result_labelqualite = await LabelQualite.find().sort('label');
-        console.log("LabelQualite ===>", result_labelqualite); //TEST
-        filters.label_qual = result_labelqualite;
-
-        let result_marques = await Product.distinct("marque");
-        result_marques = result_marques.filter(m => m.length > 0);
-        console.log("Marques produits ===>", result_marques.sort()); //TEST
-        filters.marques = result_marques;
-
-        res.json(filters);
+// Pour récupérer les produits d'un rayon après sélection de filtre(s)
+app.post("/department_products", (req, res, next) => {
+    /* try {
+        //TEST
+        console.log("id rayon", req.body.department); //TEST
+        console.log(JSON.stringify(req.body)); 
+        res.status(200).send("No problem !!");
     } catch (error) {
         error.customMsg = "Etape de récupération des filtres (partie 'Nutriscore' ou/et 'LabelQualite')";
         next(error);
-    }
+    } */
+
+
+    //TEST
+    console.log(JSON.stringify(req.body)); 
+    const id_rayon = req.body.department;
+    Product
+        .find({ rayon: id_rayon })
+        .populate("rayon")
+        .then(result => {
+            res.json(result);
+        })
+        .catch(error => { 
+            error.customMsg = "Erreur lors de l'étape de récupération des produits d'un rayon";
+            next(error);
+        });
+})
+
+
+// Pour récupérer les filtres (marge gauche qd produits affichés)
+app.get("/filters", async (req, res, next) => {
+    /* 
+    // Pour TEST pour vérifier que les prommesses s'executent de façon parallèle
+    const result_nutriscore = new Promise((resolve, reject) => {
+        setTimeout(
+            resolve, 5000, 
+            Nutriscore.find().sort('lettre').then(n => { return {"nutriscore": n} }) 
+        )
+    });
+    const result_labelqualite = new Promise((resolve, reject) => {
+        setTimeout(
+            resolve, 4000, 
+            LabelQualite.find().sort('label').then(l => { return {"label_qual": l} }) 
+        )
+    });
+    const result_marques = new Promise((resolve, reject) => {
+        setTimeout(
+            resolve, 6000,  
+            Product.distinct("marque").then(marques => {
+            return { "marques": marques.filter(m => m.length > 0).sort() }
+        }) );
+    }); 
+    */
+    const result_nutriscore = Nutriscore.find().sort('lettre').then(scores => {
+        return { "nutriscore": scores }
+    });
+    const result_labelqualite = LabelQualite.find().sort('label').then(labels => {
+        return { "label_qual": labels }
+    });
+    const result_marques = Product.distinct("marque").then(marques => {
+        return { "marques": marques.filter(m => m.length > 0).sort() }
+    });
+
+    // Execution requetes mongoDB qui sont des promesses en parallèle
+    Promise.all([
+        result_nutriscore, 
+        result_labelqualite, 
+        result_marques
+    ])
+    .then(values => {
+        let filters = {};
+        for(const result of values) {
+            // for(const [key, value] of Object.entries(result)) {
+            //    console.log("result =>", key, value); //TEST
+            //}
+            const [key, value] = Object.entries(result)[0];
+            filters[key] = value;
+        }
+        //console.log("filters >>>>>", filters); //TEST
+        res.json(filters);
+    })
+    .catch(error => {
+        error.customMsg = "Etape de récupération des filtres (partie 'Nutriscore' ou/et 'LabelQualite')";
+        next(error);
+    })
 
 });
+
+
+
 
 app.use(erreur); // Middleware de gestion des erreurs
 
