44419a94669df3565cc035e46b520c920d0b6585
SergioTrinquet <serge.trinquet@gmail.com>
2021-05-25 00:01:20 +0200

    Ajout lgn label et nutriscore + img produit français

--- App/src/components/Product.vue
+++ App/src/components/Product.vue
@@ -1,16 +1,33 @@
 <template>
     <div>   <!-- {{ dataProduct }} -->
         <div class="intitule primary-txt">{{ dataProduct.intitule }}</div>
-        <div class="marque" v-if="dataProduct.marque != ''">{{ dataProduct.marque }}</div>
+        <div class="marque" v-if="!!dataProduct.marque">{{ dataProduct.marque }}</div>
         <div class="bloc">
             <div class="descriptif">{{ dataProduct.descriptif }}</div>
             <div class="prixUnite">{{ dataProduct.prix_unite.toFixed(2) }}€ / {{ dataProduct.unite }}</div>
         </div>
-        <img alt="photo" :src="require('@/assets/imgs/' + dataProduct.nom_image + '.jpg')" class="illustration" />
+        <img alt="photo" :src="require('@/assets/imgs/produits/' + dataProduct.nom_image + '.jpg')" class="illustration" /> <!-- Faire un computed pour gérer cas ou img n'existe pas. On afficherait alors une img par défaut -->
         <!-- <img alt="photo" :src="publicPath + dataProduct.nom_image + '.jpg'" class="illustration" /> -->
         <!-- <img alt="photo" :src="require(dataProduct.imgPath)" class="illustration" /> -->
-        <div class="origine" v-if="dataProduct.origine != ''">Origine: {{ dataProduct.origine }}</div>
+        <div class="origine" v-if="!!dataProduct.origine">
+            Origine: {{ dataProduct.origine }} 
+            <span v-if="dataProduct.origine.toUpperCase() == 'FRANCE'" class="frenchFlag"></span>
+        </div>
         <div class="promotion tertiary-txt">{{ libellePromotion }}</div>
+        <div class="label_nutriscore">
+            <div class="labels">
+                <div    
+                    v-for="lb in dataProduct.label_qualite" :key="lb._id" 
+                    :class="lb._id"
+                    :data-tooltip="`label ${lb.label}`"
+                ></div>
+            </div>
+            <div 
+                v-if="!!dataProduct.nutriscore"
+                :class="['nutriscore', dataProduct.nutriscore.lettre]"
+                data-tooltip="nutriscore"
+            ></div>
+        </div>
         <div class="bottom">     
             <div>   
                 <div v-if="isDiscount" class="prix tertiary-txt">{{ dataProduct.prix_reduc.toFixed(2) }}€</div>
@@ -125,12 +142,70 @@ export default {
     font-size: 14px;
     font-style: italic;
 }
+.frenchFlag {
+    display: inline-block;
+    height: 20px;
+    /*background: 50% 50% url('../assets/imgs/flag-origine-FR.svg') no-repeat;*/
+}
 .promotion {
     font-size: 13px;
     font-weight: bold;
     background-color: #fded69;
     display: inline-block;
     padding: 0 3px;
+}
+
+.label_nutriscore {
+    display: flex;
+    justify-content: space-between;
+    margin: 3px 0 8px 0;
+    height: 25px;
+}
+.labels > div {
+    width: 25px;
+    height: 25px;
+    margin: 0 5px 0 0;
+}
+.labels > div:hover::before,
+.nutriscore:hover::before {
+    content: attr(data-tooltip);
+    position: absolute;
+    background-color: #2c3e50;
+    color: #fff;
+    font-size: 13px;
+    padding: 2px 5px;
+    border-radius: 3px;
+}
+.labels > div:hover::before {
+    margin: 33px 0 0 0;
+}
+.labels > .lb_1 {
+    background: 50% 50% url('../assets/imgs/labels/flag-label-bio.svg') no-repeat;
+}
+.labels > .lb_2 {
+    background: 50% 50% url('../assets/imgs/labels/flag-label-ecolabel.svg') no-repeat;
+}
+.labels > .lb_3 {
+    background: 50% 50% url('../assets/imgs/labels/flag-label-labelrouge.svg') no-repeat;
+}
+.labels > .lb_4 {
+    background: 50% 50% url('../assets/imgs/labels/flag-label-commerce-equitable.svg') no-repeat;
+}
+.labels > .lb_5 {
+    background: 50% 50% url('../assets/imgs/labels/flag-label-aop.svg') no-repeat;
+}
+
+.nutriscore {
+    flex-basis: 40%;
+}
+.nutriscore:hover::before {
+    margin: 33px 0 0 0;
+}
+.nutriscore.A {
+    background: 100% 50% url('../assets/imgs/nutriscore/flag-nutriscore-A.svg') no-repeat;
+}
+.nutriscore.B {
+    background: 100% 50% url('../assets/imgs/nutriscore/flag-nutriscore-B.svg') no-repeat;
 }
 
 .bottom {
