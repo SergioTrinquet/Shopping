85ad55eb810e7aee2dacc5b9f950712bf3e82090
SergioTrinquet <serge.trinquet@gmail.com>
2021-05-27 09:49:10 +0200

    Creat° composant Tags de filtres

---
+++ App/src/components/FiltersListTags.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <!-- <span class="secondary" v-for="" :key="">
+
+      </span> -->
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'FiltersListTags',
+
+    computed: {
+        /* filters() {
+            return this.$store.state.filters;
+        } */
+    }
+}
+</script>
+
+<style>
+.tag {
+    padding: 3px 8px;
+}
+</style>
