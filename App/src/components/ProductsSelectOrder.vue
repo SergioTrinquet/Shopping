02e1d3dbcf4bd0cc14f7e249239f0e6c791f6bd2
SergioTrinquet <serge.trinquet@gmail.com>
2021-07-13 11:49:40 +0200

    Réinitialisat° liste tri à chaque modif sur le state qui l'alimente + appel bon API qd sélect° tri

--- App/src/components/ProductsSelectOrder.vue
+++ App/src/components/ProductsSelectOrder.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         Trier par :
-        <select @change="sendOrderChoice" v-model="selectOrderProducts">
+        <select @change="sendOrderChoice" v-model="selectOrderProducts" id="selectTri">
             <option 
                 v-for="typeTri in listeTypeTri" :key="typeTri.id" 
                 :value="{ id: typeTri.id, champBdd: typeTri.champBdd, ordre: typeTri.ordre }"
@@ -18,16 +18,35 @@ export default {
 
     data() {
         return {
-            selectOrderProducts: { id:1, champBdd: "intitule", ordre: 1 }
+            selectOrderProducts: { id: 1, champBdd: "intitule", ordre: 1 }
         }
     },
 
     computed: {
         listeTypeTri() {
-            return this.$store.state.listeTypeTri;
+            return this.$store.state.liste_type_tri;
         },
-        queryStringParameterstoFetchProducts() {
-            return this.$store.getters.getQueryStringParametersToFetchProducts;
+
+        actionName() {      //console.log('GETTER actionName', this.$store.getters.getGoodActionName); //TEST
+            return this.$store.getters.getGoodActionName;
+        }
+    },
+
+    watch: {
+        // "immediate: true" pour que ce watch soit executé lors de l'affectation initiale du computed correspondant (a la creation du composant), sinon pas executé.
+        listeTypeTri: {
+            immediate: true,
+            handler() {
+                this.$nextTick(() => {
+                    const selectTri = document.querySelector("#selectTri");
+
+                    selectTri.selectedIndex = 0; // Qd modif sur nbr d'option dans le select Tri : Sélect° sur 1ere option...
+
+                    // ...puis on déclenche l'evenement 'change' sur le select du Tri pour appliquer le Tri en quest° sur les produits
+                    const e = new Event("change");
+                    selectTri.dispatchEvent(e);
+                });
+            }
         }
     },
 
@@ -35,14 +54,14 @@ export default {
         sendOrderChoice() {
             const searchParams = new URLSearchParams(this.selectOrderProducts);
 
-            // Enregistrement value du classement sélectionné dans le store
-            // NOTE: Fonctionne aussi en passant 'searParams' sans le convertir en string via (.toString()) mais on évite car ds ce cas valeur pas lisible avec 'Vue.js devtools'
+            // Enregistrement ds le store de la value du classement sélectionné sous forme de chaine de requête. Va mettre à jour la chaine de requete globale (filtres + tri + type de recherche) construite ds getter 'getQueryStringParameterstoFetchProducts'
+            // NOTE: Fonctionne aussi en passant 'searchParams' sans le convertir en string via (.toString()) mais on évite car ds ce cas valeur pas lisible avec 'Vue.js devtools'
             this.$store.commit('SET_TRI_QUERY_STRING_PARAMETERS', searchParams.toString());
-            
-            //console.log("sélection (GET) => ", this.queryStringParameterstoFetchProducts); //TEST
-            
-            // Appel API pour récup. des produits à afficher dans le bon ordre
-            this.$store.dispatch('fetchProductsDepartment', this.queryStringParameterstoFetchProducts);
+
+            // Appel API pour récup. des produits à afficher ds l'ordre du tri sélectionné : 
+            // Le getter 'this.actionName' correspond soit à 'fetchProductsDepartment' qd produits affichés suite à recherche par rayon,
+            // soit à 'fetchProductsValidationSearchEngine' qd produits affichés suite à recherche via moteur de recherche
+            this.$store.dispatch(this.actionName);
         }
     }
 }
