6ef11499862471fdcab2e009621fdcfff5ece2d0
SergioTrinquet <serge.trinquet@gmail.com>
2021-07-15 17:35:05 +0200

    watch en immediate suite à chargement du composant avec v-if

--- App/src/components/FiltersListTags.vue
+++ App/src/components/FiltersListTags.vue
@@ -40,95 +40,99 @@ export default {
 
     watch: {
         // Pour afficher le bon libellé ds le tag + récupérer paramètres qui seront apel&s qd suppress° d'un tag
-        filters_query_string_parameters(val) {
-            const searchParams = new URLSearchParams(val);
+        filters_query_string_parameters: {          
+            immediate: true,
+            handler(val) {
+                const searchParams = new URLSearchParams(val);
 
-            this.listeTags = [];
-            searchParams.forEach((value, key) => {
-                console.log("FiltersListags", key, value); //TEST
+                this.listeTags = [];
+                searchParams.forEach((value, key) => {
+                    console.log("FiltersListags", key, value); //TEST
 
-                if(key == 'nutriscore') {
-                    const n = this.filters.nutriscore.filter(n => {
-                        if(n.id === value) return n;
-                    });
-                    this.listeTags.push({ 
-                        libelle: `nutriscore ${n[0].lettre}`, 
-                        queryStringParam: key, 
-                        queryStringValue: value 
-                    });
-
-                } else if(key == 'label_qualite') {
-                    const l = this.filters.label_qual.filter(l => {
-                        if(l.id === value) return l;
-                    });
-                    this.listeTags.push({ 
-                        libelle: l[0].libelle, 
-                        queryStringParam: key, 
-                        queryStringValue: value 
-                    });
-
-                } else if(key == 'prdsFr') {
-                    this.listeTags.push({ 
-                        libelle: 'produits français', 
-                        queryStringParam: key 
-                    });
-
-                } else if(key == 'promos') {
-                    this.listeTags.push({ 
-                        libelle: 'promotions', 
-                        queryStringParam: key 
-                    });
-
-                } else { // Marques
-                    this.listeTags.push({ 
-                        libelle: value, 
-                        queryStringParam: key, 
-                        queryStringValue: value 
-                    });
-                }
-
-                /* switch(key) {
-                    case 'nutriscore':
+                    if(key == 'nutriscore') {
                         const n = this.filters.nutriscore.filter(n => {
                             if(n.id === value) return n;
-                        })
+                        });
                         this.listeTags.push({ 
                             libelle: `nutriscore ${n[0].lettre}`, 
                             queryStringParam: key, 
                             queryStringValue: value 
                         });
-                        break;
-                    case 'label_qualite':
+
+                    } else if(key == 'label_qualite') {
                         const l = this.filters.label_qual.filter(l => {
                             if(l.id === value) return l;
-                        })
+                        });
                         this.listeTags.push({ 
-                            libelle: l[0].label, 
+                            libelle: l[0].libelle, 
                             queryStringParam: key, 
                             queryStringValue: value 
                         });
-                        break;
-                    case 'prdsFr':
+
+                    } else if(key == 'prdsFr') {
                         this.listeTags.push({ 
                             libelle: 'produits français', 
                             queryStringParam: key 
                         });
-                        break;
-                    case 'promos':
+
+                    } else if(key == 'promos') {
                         this.listeTags.push({ 
                             libelle: 'promotions', 
                             queryStringParam: key 
                         });
-                        break;
-                    case 'marque':
+
+                    } else { // Marques
                         this.listeTags.push({ 
                             libelle: value, 
                             queryStringParam: key, 
                             queryStringValue: value 
                         });
-                        break;
-                } */
-            })
+                    }
+
+                    /* switch(key) {
+                        case 'nutriscore':
+                            const n = this.filters.nutriscore.filter(n => {
+                                if(n.id === value) return n;
+                            })
+                            this.listeTags.push({ 
+                                libelle: `nutriscore ${n[0].lettre}`, 
+                                queryStringParam: key, 
+                                queryStringValue: value 
+                            });
+                            break;
+                        case 'label_qualite':
+                            const l = this.filters.label_qual.filter(l => {
+                                if(l.id === value) return l;
+                            })
+                            this.listeTags.push({ 
+                                libelle: l[0].label, 
+                                queryStringParam: key, 
+                                queryStringValue: value 
+                            });
+                            break;
+                        case 'prdsFr':
+                            this.listeTags.push({ 
+                                libelle: 'produits français', 
+                                queryStringParam: key 
+                            });
+                            break;
+                        case 'promos':
+                            this.listeTags.push({ 
+                                libelle: 'promotions', 
+                                queryStringParam: key 
+                            });
+                            break;
+                        case 'marque':
+                            this.listeTags.push({ 
+                                libelle: value, 
+                                queryStringParam: key, 
+                                queryStringValue: value 
+                            });
+                            break;
+                    } */
+                })
+
+            }
         }
     }
 }
