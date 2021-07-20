35b1cb4545b78047f7c99db03769978b9fec8128
SergioTrinquet <serge.trinquet@gmail.com>
2021-07-20 10:22:28 +0200

    Creat° nv composant autocomplete à parti de SearchEngine.vue pour permettre lazy loading conditionnel

--- App/src/components/SearchEngine.vue
+++ App/src/components/SearchEngine.vue
@@ -20,31 +20,20 @@
             @click="searchProducts"
         />
 
-        <transition name="autocomplete">
-          <div 
-            id="proposals" 
-            class="proposals primary-txt" 
-            v-if="displaySearchEngineResults"
-          >
-            <div class="lgnProduit legende">
-              <div class="intituleProduit">Intitule produit</div>
-              <div class="marqueProduit">Marque</div>
-            </div>
-            <div class="lgnProduit" 
-              v-for="result in autocompleteResults" :key="result._id"
-              @click="displayProduct(result._id)"
-            >
-              <div class="intituleProduit" v-html="highlight(result, 'intitule')"></div>
-              <div class="marqueProduit" v-html="highlight(result, 'marque')"></div>{{result.score}}
-            </div>
-          </div>
-        </transition>
+        <Autocomplete v-if="displaySearchEngineResults" />
+
     </div>
 </template>
 
 <script>
+const Autocomplete = () => import(/* webpackChunkName: "SearchEngineAutocomplete" */ '@/components/SearchEngineAutocomplete')
+
 export default {
-    name: 'GlobalSearchEngine',
+    name: 'SearchEngine',
+
+    components: {
+        Autocomplete
+    },
 
     data() {
         return {
@@ -84,7 +73,6 @@ export default {
 
       // Qd recherche par moteur de rech., ajout/retrait ds liste déroulante 'Tri' d'une option 'pertinence'
       searchBySearchString(val) {
-        console.warn("WATCH de 'searchBySearchString'", val); //TEST
         this.$store.commit(val ? 'ADD_LISTE_TRI_OPTION' : 'REMOVE_LISTE_TRI_OPTION');
       }
     },
@@ -105,7 +93,7 @@ export default {
           } else {
             // Pour limiter le nbr de requetes : Déclenchement action 200ms après frappe et suppression de l'appel si nvelle frappe a lieu avant ce délai de 200ms
             clearTimeout(this.timer); // Suppression des évènements 'keyup' précédent 
-            this.timer = setTimeout(() => this.$store.dispatch('fetchProductsForAutocompleteSearchEngine', new_saisie), 200); // Déclenchement action après 200ms
+            this.timer = setTimeout(() => this.$store.dispatch('fetchProductsForAutocomplete', new_saisie), 200); // Déclenchement action après 200ms
           }
         }
         
@@ -130,39 +118,6 @@ export default {
         if(this.componentsOpen) {
           this.$store.dispatch("closeComponents");
         }
-      },
-
-
-      highlight(val, champ) {
-        const goodPath = val.highlight.filter(p => p.path == champ)[0];
-        
-        if(!!goodPath == false) {
-          return val[champ];
-        } else {
-          let txt = "";
-          goodPath.texts.forEach(str => {
-            if(str.type == "hit") {
-              txt += `<i ${this.$options._scopeId}>${str.value}</i>` // Ici 'this.$options._scopeId' correspond à l'attribut rajouté par Vue.js pour scoper le CSS. Ajouté car autrement CSS ds la partie 'style' ne fonctionnera pas
-            } else {
-              txt += str.value
-            }
-          });
-          return txt;
-        }
-      },
-
-      // Qd click sur un produit ds l'autocomplete
-      displayProduct(id) {
-        this.reinitialisationFiltresEtTri();
-        this.$store.dispatch('fetchProductFromAutocompleteSearchEngine', id);
-      },
-
-      // Pour réinitialiser les filtres si l'utilisateur avait choisi au pralable de voir les produits d'un rayon, ainsi que les filtres et le tri qu'il a pu sélectionner
-      reinitialisationFiltresEtTri() {
-        this.$store.commit('SET_FILTERS', {});
-        //this.$store.commit('SET_TYPE_OF_SEARCH_PRODUCTS', {});
-        this.$store.commit('SET_FILTERS_QUERY_STRING_PARAMETERS', "");
-        this.$store.commit('SET_TRI_QUERY_STRING_PARAMETERS', "");
       },
 
 
@@ -196,7 +151,8 @@ export default {
             && 
             (
               e.target.id == "proposals" || 
-              e.target.id == "searchEngine" || Array.from(document.querySelector("#searchEngine").children).includes(e.target) 
+              e.target.id == "searchEngine" || 
+              Array.from(document.querySelector("#searchEngine").children).includes(e.target) 
             )
           ) 
         {
@@ -260,77 +216,5 @@ export default {
 }
 #iconSearch:hover {
   color: rgb(24, 136, 196);
-}
-
-#proposals {
-  position: absolute;
-  width: 100%;
-  box-sizing: border-box;
-  border-radius: 4px;
-  margin: 30px 0 0 -8px;
-  padding: 0;
-  box-shadow: 0 0 3px rgba(0,0,0,0.7);
-  background-color: #fff;
-}
-
-#proposals .lgnProduit {
-  display: flex;
-  font-size: 14px;
-  padding: 5px;
-  border-bottom: dotted 1px #254A7B;
-  margin: 0 4px;
-}
-#proposals .lgnProduit:last-child {
-  border-bottom-width: 0;
-}
-#proposals .lgnProduit:not(.legende):hover {
-  background-color: rgba(48,93,154, 0.05);
-  cursor: pointer;
-}
-#proposals .lgnProduit > div {
-  flex-grow: 1;
-  flex-basis: 0;
-}
-#proposals .lgnProduit.legende {  
-  font-size: 13px;
-  border-bottom: solid 1px #254A7B;
-  background-color: #2F5C99;
-  color: #fff;
-  border-radius: 4px 4px 0 0;
-  padding: 4px 10px 2px 10px;
-  margin: 0;
-}
-#proposals .lgnProduit i {
-    font-style: normal;
-    padding: 0 3px;
-
-    background-color: rgb(255, 255, 0);
-    /* background-color: pink; */
-}
-
-#proposals:after {
-	bottom: 100%;
-	left: 50%;
-	border: solid transparent;
-	content: "";
-	height: 0;
-	width: 0;
-	position: absolute;
-	pointer-events: none;
-	border-color: rgba(213, 0, 0, 0);
-	border-bottom-color: #2F5C99;
-	border-width: 10px;
-	margin-left: -10px;
-}
-
-
-.autocomplete-enter,
-.autocomplete-leave-to {
-  opacity: 0;
-  transform: translateY(-15px);
-}
-.autocomplete-enter-active,
-.autocomplete-leave-active {
-  transition: all 0.25s ease-out;
 }
 </style>
---
+++ App/src/components/SearchEngineAutocomplete.vue
@@ -0,0 +1,140 @@
+<template>
+    <transition name="autocomplete" appear>
+        <div 
+            id="proposals" 
+            class="proposals primary-txt" 
+        >
+        <div class="lgnProduit legende">
+            <div class="intituleProduit">Intitule produit</div>
+            <div class="marqueProduit">Marque</div>
+        </div>
+        <div class="lgnProduit" 
+            v-for="result in autocompleteResults" :key="result._id"
+            @click="displayProduct(result._id)"
+        >
+            <div class="intituleProduit" v-html="highlight(result, 'intitule')"></div>
+            <div class="marqueProduit" v-html="highlight(result, 'marque')"></div>{{result.score}}
+        </div>
+        </div>
+    </transition>
+</template>
+
+<script>
+export default {
+    name: 'SearchEngineAutocomplete',
+
+    computed: {
+      autocompleteResults() {
+        return this.$store.state.autocompleteResults;
+      }
+    },
+
+    methods: {
+        // Qd click sur un produit ds l'autocomplete
+        displayProduct(id) {
+            this.reinitialisationFiltresEtTri();
+            this.$store.dispatch('fetchProductFromAutocomplete', id);
+        },
+
+        // Pour réinitialiser les filtres si l'utilisateur avait choisi au pralable de voir les produits d'un rayon, ainsi que les filtres et le tri qu'il a pu sélectionner
+        reinitialisationFiltresEtTri() {
+            this.$store.commit('SET_FILTERS', {});
+            this.$store.commit('SET_FILTERS_QUERY_STRING_PARAMETERS', "");
+            this.$store.commit('SET_TRI_QUERY_STRING_PARAMETERS', "");
+        },
+
+        highlight(val, champ) {
+            const goodPath = val.highlight.filter(p => p.path == champ)[0];
+            
+            if(!!goodPath == false) {
+                return val[champ];
+            } else {
+                let txt = "";
+                goodPath.texts.forEach(str => {
+                    if(str.type == "hit") {
+                    txt += `<i ${this.$options._scopeId}>${str.value}</i>` // Ici 'this.$options._scopeId' correspond à l'attribut rajouté par Vue.js pour scoper le CSS. Ajouté car autrement CSS ds la partie 'style' ne fonctionnera pas
+                    } else {
+                    txt += str.value
+                    }
+                });
+                return txt;
+            }
+        }
+    }
+
+}
+</script>
+
+<style>
+.autocomplete-enter,
+.autocomplete-leave-to {
+  opacity: 0;
+  transform: translateY(-15px);
+}
+.autocomplete-enter-active,
+.autocomplete-leave-active {
+  transition: all 0.25s ease-out;
+}
+
+
+#proposals {
+  position: absolute;
+  width: 100%;
+  box-sizing: border-box;
+  border-radius: 4px;
+  margin: 30px 0 0 -8px;
+  padding: 0;
+  box-shadow: 0 0 3px rgba(0,0,0,0.7);
+  background-color: #fff;
+}
+
+#proposals .lgnProduit {
+  display: flex;
+  font-size: 14px;
+  padding: 5px;
+  border-bottom: dotted 1px #254A7B;
+  margin: 0 4px;
+}
+#proposals .lgnProduit:last-child {
+  border-bottom-width: 0;
+}
+#proposals .lgnProduit:not(.legende):hover {
+  background-color: rgba(48,93,154, 0.05);
+  cursor: pointer;
+}
+#proposals .lgnProduit > div {
+  flex-grow: 1;
+  flex-basis: 0;
+}
+#proposals .lgnProduit.legende {  
+  font-size: 13px;
+  border-bottom: solid 1px #254A7B;
+  background-color: #2F5C99;
+  color: #fff;
+  border-radius: 4px 4px 0 0;
+  padding: 4px 10px 2px 10px;
+  margin: 0;
+}
+#proposals .lgnProduit i {
+    font-style: normal;
+    padding: 0 3px;
+
+    background-color: rgb(255, 255, 0);
+    /* background-color: pink; */
+}
+
+#proposals:after {
+	bottom: 100%;
+	left: 50%;
+	border: solid transparent;
+	content: "";
+	height: 0;
+	width: 0;
+	position: absolute;
+	pointer-events: none;
+	border-color: rgba(213, 0, 0, 0);
+	border-bottom-color: #2F5C99;
+	border-width: 10px;
+	margin-left: -10px;
+}
+</style>
