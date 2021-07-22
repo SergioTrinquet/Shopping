5110fc5639adf771986c9f77f8865894dc93a232
SergioTrinquet <serge.trinquet@gmail.com>
2021-07-22 16:50:54 +0200

    Creat° nv composant comme view par defaut

---
+++ App/src/views/Accueil.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="center">
+      <div class="accueilTexte">Bienvenue sur mon appli de courses en ligne!</div>
+      <img 
+        src="../assets/imgs/illustration_undraw_shopping_app.svg" 
+        alt="Bienvenue sur mon appli de courses en ligne!" 
+        class="accueilSVG" 
+      />
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 60px);
+}
+.accueilTexte {
+  font-size: 3vw;
+  line-height: 3.5vw;
+  position: absolute;
+  width: 40vw;
+  min-width: 250px;
+  max-width: 350px;
+  text-align: center;
+  margin: -30vh 0 0 0;
+}
+@media screen and (max-width: 600px) {
+  .accueilTexte {
+    font-size: 20px;
+    line-height: 25px;
+  }
+}
+@media screen and (min-width: 1000px) {
+  .accueilTexte {
+    font-size: 30px;
+    line-height: 35px;
+  }
+}
+.accueilSVG {
+  width: 30vw;
+  min-width: 250px;
+  max-width: 350px;
+}
+</style>
