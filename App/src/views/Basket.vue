129184afb01cf0f5b7036891808ba2cd69f6e0b6
SergioTrinquet <serge.trinquet@gmail.com>
2021-07-23 11:23:37 +0200

    Modif mineure

--- App/src/views/Basket.vue
+++ App/src/views/Basket.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <h1>Mon panier</h1>
+    <div>Page à faire dans un 2eme temps (après authentification créée et enregistrements commandes ds mongoDB)</div>
   </div>
 </template>
