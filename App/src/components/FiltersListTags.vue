95ede5e436b2ad2099a1837b516257f07d968a67
SergioTrinquet <serge.trinquet@gmail.com>
2021-07-28 11:37:59 +0200

    Cleaning code

--- App/src/components/FiltersListTags.vue
+++ App/src/components/FiltersListTags.vue
@@ -47,7 +47,6 @@ export default {
 
                 this.listeTags = [];
                 searchParams.forEach((value, key) => {
-                    console.log("FiltersListags", key, value); //TEST
 
                     if(key == 'nutriscore') {
                         const n = this.filters.nutriscore.filter(n => {
@@ -89,47 +88,6 @@ export default {
                         });
                     }
 
-                    /* switch(key) {
-                        case 'nutriscore':
-                            const n = this.filters.nutriscore.filter(n => {
-                                if(n.id === value) return n;
-                            })
-                            this.listeTags.push({ 
-                                libelle: `nutriscore ${n[0].lettre}`, 
-                                queryStringParam: key, 
-                                queryStringValue: value 
-                            });
-                            break;
-                        case 'label_qualite':
-                            const l = this.filters.label_qual.filter(l => {
-                                if(l.id === value) return l;
-                            })
-                            this.listeTags.push({ 
-                                libelle: l[0].label, 
-                                queryStringParam: key, 
-                                queryStringValue: value 
-                            });
-                            break;
-                        case 'prdsFr':
-                            this.listeTags.push({ 
-                                libelle: 'produits français', 
-                                queryStringParam: key 
-                            });
-                            break;
-                        case 'promos':
-                            this.listeTags.push({ 
-                                libelle: 'promotions', 
-                                queryStringParam: key 
-                            });
-                            break;
-                        case 'marque':
-                            this.listeTags.push({ 
-                                libelle: value, 
-                                queryStringParam: key, 
-                                queryStringValue: value 
-                            });
-                            break;
-                    } */
                 })
 
             }
