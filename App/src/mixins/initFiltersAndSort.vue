5e8bb390be5e9fd0a9f887858e485ac1751a4dd4
SergioTrinquet <serge.trinquet@gmail.com>
2021-09-02 16:57:23 +0200

    Creat° mixin pour reinitialiser Filtres et Tri

---
+++ App/src/mixins/initFiltersAndSort.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    methods: {
+        initFiltersAndSort() {
+            this.$store.commit('SET_FILTERS', {});
+            this.$store.commit('SET_FILTERS_QUERY_STRING_PARAMETERS', "");
+            this.$store.commit('SET_TRI_QUERY_STRING_PARAMETERS', "");
+        }
+    }
+}
+</script>
